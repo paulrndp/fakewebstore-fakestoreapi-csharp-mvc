bd16c0b781cbd832bf4525a5636870dbd7e92140
paulrndp <paulr@DESKTOP-47REJSO>
2022-08-24 15:37:55 +0800

    commit : fetch data from api

--- FakeWebStore/Controllers/HomeController.cs
+++ FakeWebStore/Controllers/HomeController.cs
@@ -1,15 +1,33 @@
 ﻿using FakeWebStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System;
+using System.Net.Http;
+using Newtonsoft;
+using Newtonsoft.Json;
 
 namespace FakeWebStore.Controllers
 {
     public class HomeController : Controller
     {
+        Uri baseAddress = new Uri("https://fakestoreapi.com/products");
+        HttpClient client;
 
+        public HomeController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Product> modelList = new List<Product>();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                modelList = JsonConvert.DeserializeObject<List<Product>>(data);
+            }
+            return View(modelList);
         }
         public IActionResult Privacy()
         {
---
+++ FakeWebStore/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace FakeWebStore.Models
+{
+
+    public class Product
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public float price { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string image { get; set; }
+        public Rating rating { get; set; }
+    }
+
+    public class Rating
+    {
+        public float rate { get; set; }
+        public int count { get; set; }
+    }
+
+}
