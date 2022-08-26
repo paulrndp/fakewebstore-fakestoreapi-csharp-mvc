b6056ace3e62a04deb110ae5befc85fb948811cb
paulrndp <paulr@DESKTOP-47REJSO>
2022-08-26 14:49:08 +0800

    commit : edit product

--- FakeWebStore/Controllers/HomeController.cs
+++ FakeWebStore/Controllers/HomeController.cs
@@ -5,9 +5,10 @@ using System;
 using System.Net.Http;
 using Newtonsoft;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace FakeWebStore.Controllers
-{
+{  
     public class HomeController : Controller
     {
         Uri baseAddress = new Uri("https://fakestoreapi.com/products");
@@ -28,6 +29,31 @@ namespace FakeWebStore.Controllers
                 modelList = JsonConvert.DeserializeObject<List<Product>>(data);
             }
             return View(modelList);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Product model = new Product();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                model = JsonConvert.DeserializeObject<Product>(data);
+            }
+            return View(model);
+        }
+        [HttpPatch]
+        public IActionResult Edit(Product obj)
+        {
+            string data = JsonConvert.SerializeObject(obj);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = client.PutAsync(client.BaseAddress + "/" + obj.id, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
         }
         public IActionResult Privacy()
         {
